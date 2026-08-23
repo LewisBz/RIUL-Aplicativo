6f574810c04d3b54cfd18d0977c59786760fcc88
HANDR\handr <handreslperez@gmail.com>
2026-08-22 19:46:54 -0500

    cambio en el env de las migraciones

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -37,7 +37,7 @@ def get_metadata():
 def run_migrations_offline():
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=get_metadata(), literal_binds=True, compare_type=True
+        url=url, target_metadata=get_metadata(), literal_binds=True
     )
 
     with context.begin_transaction():
@@ -59,7 +59,6 @@ def run_migrations_online():
             connection=connection,
             target_metadata=get_metadata(),
             process_revision_directives=process_revision_directives,
-            compare_type=True,
             **current_app.extensions["migrate"].configure_args,
         )
 
